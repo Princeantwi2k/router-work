166a327f6cbfdc93b3c8ee0cb3adbf687ef2526b
prince antwi <antwi2k@gmail.com>
2021-07-27 13:35:50 +0800

    added About.jsx

---
+++ src/Pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: `center` }}>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum iusto
+        corrupti odit, assumenda perspiciatis eum iure obcaecati nesciunt natus
+        quam sint modi rerum facilis, consequatur nam doloremque incidunt
+        ratione dolor.
+      </p>
+    </div>
+  );
+};
+
+export default About;
