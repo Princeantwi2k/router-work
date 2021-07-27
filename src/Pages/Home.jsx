feff7a4335152abab3c18cecf28d84f65d74ebb3
prince antwi <antwi2k@gmail.com>
2021-07-27 13:33:56 +0800

    add home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: `center` }}>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+        delectus harum expedita velit maiores ipsa ex magnam facilis neque,
+        similique id qui iure amet fugiat necessitatibus eius voluptatibus
+        doloremque laborum?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
