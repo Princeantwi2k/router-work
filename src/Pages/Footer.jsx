d5b1361aee991c860a1df4878e90649445147b72
prince antwi <antwi2k@gmail.com>
2021-07-27 13:35:15 +0800

    added Footer.jsx

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: `black`,
+          color: `white`,
+          textAlign: `center`,
+          marginTop: `430px`,
+        }}
+      >
+        Footer
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
