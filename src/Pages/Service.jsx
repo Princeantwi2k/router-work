314966407ffe181d1494019e4974b5a2549c7cd4
prince antwi <antwi2k@gmail.com>
2021-07-27 13:36:37 +0800

    added Service.jsx

---
+++ src/Pages/Service.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: `center` }}>Service Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis
+        provident repudiandae asperiores voluptates qui magni autem, dolorum
+        quaerat cumque ipsum numquam iusto, rerum quas fugiat, dolore
+        voluptatibus itaque atque laborum!
+      </p>
+    </div>
+  );
+};
+
+export default Service;
