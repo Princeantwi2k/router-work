6270bfee1f33f01ef2b816e552f2bf4d0470d762
prince antwi <antwi2k@gmail.com>
2021-07-27 13:36:17 +0800

    added Contact.jsx

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: `center` }}>Contact Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis facere
+        quisquam aperiam tenetur nobis quod ad eaque qui laudantium quia,
+        tempore ipsam earum at quam est fugit sint numquam debitis.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
